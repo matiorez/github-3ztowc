fc02abc3825ffee63296f5ef57b4d1fe2d999226
matiorez <mati.hyk@wp.pl>
2021-09-16 14:07:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-3ztowc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-3ztowc)
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { MovieCoverComponent } from './shared/movie-cover/movie-cover.component';
+import { MovieDetailsComponent } from './pages/movies/movie-details/movie-details.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { MoviesInCategoryComponent } from './pages/categories/movies-in-category/movies-in-category.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MovieDetailsComponent,
+    MoviesComponent,
+    CategoriesComponent,
+    PageNotFoundComponent,
+    MoviesInCategoryComponent,
+    MovieCoverComponent,
+  ],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
